29006625b397e6f0bdc41dcd3c26fde7b1c45e0b
Magda Skopczynska <madstar@interia.pl>
2018-10-08 19:38:35 +0200

    using THS keyword

--- test.js
+++ test.js
@@ -9,9 +9,17 @@ var myCar2 = {
 
     maxSpeed: 70,
     driver: "Net Ninja",
-    drive: function(speed, time){console.log(speed * time);}
-
+    drive: function(speed, time){
+        console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    },
+    test: function(){
+        console.log(this);
+    }
 };
-
+myCar2.test();
+myCar2.logDriver ();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
